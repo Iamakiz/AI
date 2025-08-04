d188e81e45b79f30de9dacc8b6f28ecc45eeace4
Amanda <160647962+Iamakiz@users.noreply.github.com>
2025-08-04 15:04:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,12 @@ const perguntas = [
     ],
   },
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
